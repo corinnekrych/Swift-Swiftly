2907539c5236c0fd7fbb366d93d193a5e1342267
corinnekrych <corinnekrych@gmail.com>
2014-07-18 17:26:10 +0200

    Optionals playground

--- Tuple.playground/section-1.swift
+++ Tuple.playground/section-1.swift
@@ -38,10 +38,25 @@ namedTuple.message == namedTuple.1
 var dict = ["onekey":"onevalue", "twokey":"twovalue"]
 
 for (key, value) in dict {
-    dict[key] = "tttt"
+    dict[key] = "assign-me-sth"
     println("\(key):\(value)")
 }
 dict
+
+// Tuple in switch statement
+var imageName: String
+var condition = 310
+var nightTime = true
+switch (condition, nightTime) {
+case let (x, y) where x < 300 && y == true:     imageName = "11n"
+case let (x, y) where x < 300 && y == false:    imageName = "11d"
+    
+case let (x, y) where x < 1000 && y == true:    imageName = "11n"
+case let (x, y) where x < 1000 && y == false:   imageName = "11d"
+    
+case let (x, y): imageName = "dunno"
+}
+
 
 // Array of tuple and issue with beta3
 
